65e8b8916272a75a6baed4ee6f3242bb6d670a26
CapSap <cshotam@gmail.com>
2024-05-15 20:33:02 +1000

    add some inital state

--- src/app/create-request/page.tsx
+++ src/app/create-request/page.tsx
@@ -2,8 +2,31 @@
 
 import { useState } from "react";
 
+type item = {
+  sku: string;
+  quantity: string;
+  description: string;
+};
+
 export default function CreateRequestPage() {
-  const [customerName, setCustomerName] = useState();
+  const [name, setName] = useState();
+  const [phone, setPhone] = useState();
+  const [email, setEmail] = useState();
+  const [address, setAddress] = useState();
+  const [items, setItems] = useState<item[]>([]);
+
+  function handleGetMoreItems() {
+    setItems((prevState) => {
+      return [
+        ...prevState,
+        {
+          sku: "",
+          quantity: "",
+          description: "",
+        },
+      ];
+    });
+  }
 
   return (
     <div>
@@ -43,34 +66,48 @@ export default function CreateRequestPage() {
             className="resize rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
         </div>
-        request info
-        <div>
-          <label htmlFor="quantity">Quantity</label>
-          <input
-            type="text"
-            name="quantity"
-            id="quantity"
-            className="rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
+        Item info
+        <div className="md:col-span-2 bg-slate-300 flex flex-col justify-center items-center">
+          <p>Request more items: </p>
+          <button
+            type="button"
+            onClick={() => handleGetMoreItems()}
+            className="inline-block bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"
+          >
+            Get more requests
+          </button>
         </div>
-        <div>
-          <label htmlFor="sku">SKU</label>
-          <input
-            type="text"
-            name="sku"
-            id="sku"
-            className="rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
-        </div>
-        <div>
-          <label htmlFor="decription">Description</label>
-          <input
-            type="text"
-            name="description"
-            id="description"
-            className="rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
-        </div>
+        {items.map((item, i) => (
+          <div key={"item" + i}>
+            <div>
+              <label htmlFor="quantity">Quantity</label>
+              <input
+                type="text"
+                name="quantity"
+                id="quantity"
+                className="rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div>
+              <label htmlFor="sku">SKU</label>
+              <input
+                type="text"
+                name="sku"
+                id="sku"
+                className="rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div>
+              <label htmlFor="decription">Description</label>
+              <input
+                type="text"
+                name="description"
+                id="description"
+                className="rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+        ))}
       </form>
     </div>
   );
