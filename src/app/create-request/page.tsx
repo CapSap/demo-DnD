3f1d25c83e1adfdae8c7cc0eb39381d53e794750
CapSap <cshotam@gmail.com>
2024-05-21 00:23:23 +1000

    wip

--- src/app/create-request/page.tsx
+++ src/app/create-request/page.tsx
@@ -1,11 +1,15 @@
 import CreateRequestForm from "../components/CreateRequestForm";
-import dbConnect from "../utils/dbConnect";
+import { createStoreRequest, dbConnect } from "../utils/dbConnect";
 
 export default function CreateRequestPage() {
-  const client = dbConnect();
+  async function createRequest() {
+    "use server";
+    await createStoreRequest({});
+  }
+
   return (
     <div>
-      <CreateRequestForm />
+      <CreateRequestForm createStoreRequest={createStoreRequest} />
     </div>
   );
 }
