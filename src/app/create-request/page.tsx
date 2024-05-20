9bb87630684d4a02c8238bd69c0647d3b5211d73
CapSap <cshotam@gmail.com>
2024-05-21 00:24:50 +1000

    remove old createRequest func that was created in parent page component

--- src/app/create-request/page.tsx
+++ src/app/create-request/page.tsx
@@ -2,11 +2,6 @@ import CreateRequestForm from "../components/CreateRequestForm";
 import { createStoreRequest, dbConnect } from "../utils/dbConnect";
 
 export default function CreateRequestPage() {
-  async function createRequest() {
-    "use server";
-    await createStoreRequest({});
-  }
-
   return (
     <div>
       <CreateRequestForm createStoreRequest={createStoreRequest} />
