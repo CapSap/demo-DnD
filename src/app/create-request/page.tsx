bb106b2e4e438406c88ad3601c7f429e7ef163fa
CapSap <cshotam@gmail.com>
2024-05-24 13:54:40 +1000

    have a function in the parent server component to handle the response of writing to db

--- src/app/create-request/page.tsx
+++ src/app/create-request/page.tsx
@@ -1,7 +1,20 @@
 import CreateRequestForm from "../components/CreateRequestForm";
-import { createStoreRequest, dbConnect } from "../utils/dbConnect";
+import { createStoreRequest as dbCreate, dbConnect } from "../utils/dbConnect";
 
 export default function CreateRequestPage() {
+  async function createStoreRequest(payload) {
+    "use server";
+    const response = await dbCreate(payload);
+
+    if (response.error) {
+      // Display a user-friendly error message to the user
+      return `Error creating store request: ${response.error.message}. Details: ${response.error.details}`;
+    }
+
+    // Handle success response
+    return `Store request created successfully!`;
+  }
+
   return (
     <div>
       <CreateRequestForm createStoreRequest={createStoreRequest} />
