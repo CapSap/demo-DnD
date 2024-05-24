39dd8baa51700f350191cb4b1c385c82900f33bd
CapSap <cshotam@gmail.com>
2024-05-24 13:54:55 +1000

    remove comment

--- src/app/create-request/page.tsx
+++ src/app/create-request/page.tsx
@@ -7,11 +7,9 @@ export default function CreateRequestPage() {
     const response = await dbCreate(payload);
 
     if (response.error) {
-      // Display a user-friendly error message to the user
       return `Error creating store request: ${response.error.message}. Details: ${response.error.details}`;
     }
 
-    // Handle success response
     return `Store request created successfully!`;
   }
 
