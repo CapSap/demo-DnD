f1245e3b0b272be88e0016447df42931c71f9171
CapSap <cshotam@gmail.com>
2024-06-07 12:41:11 +1000

    wip styling

--- src/app/components/CreateRequestForm.tsx
+++ src/app/components/CreateRequestForm.tsx
@@ -56,7 +56,7 @@ export default function CreateRequestForm({
       | React.ChangeEvent<HTMLInputElement>
       | React.ChangeEvent<HTMLTextAreaElement>
       | React.ChangeEvent<HTMLSelectElement>,
-    index: number
+    index: number,
   ) {
     setItems((prevState) => {
       const updatedRequest = {
@@ -81,38 +81,36 @@ export default function CreateRequestForm({
           handleFormSubmit();
         }}
       >
-        <div>
-          <div>
-            <label
-              className="inline-block text-gray-800 text-sm sm:text-base mb-2"
-              htmlFor="requestingStore"
-            >
-              Requesting store:
-            </label>
-            <select
-              className=" bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
-              id={"requestingStore"}
-              name="requestingStore"
-              key={"requestingStore"}
-              value={requestingStore}
-              onChange={(e) => {
-                setRequestingStore(e.target.value);
-              }}
-            >
-              <option value={"default"} disabled={true}>
-                Please choose an option
-              </option>
+        <div className="flex flex-col">
+          <label
+            className="flex flex-col text-sm text-gray-800 sm:text-base"
+            htmlFor="requestingStore"
+          >
+            Requesting store:
+          </label>
+          <select
+            className="rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+            id={"requestingStore"}
+            name="requestingStore"
+            key={"requestingStore"}
+            value={requestingStore}
+            onChange={(e) => {
+              setRequestingStore(e.target.value);
+            }}
+          >
+            <option value={"default"} disabled={true}>
+              Please choose an option
+            </option>
 
-              <option value="213">Canberra - 213</option>
-              <option value="416">Fortitude Valley - 416</option>
-              <option value="710">Hobart - 710</option>
-              <option value="314">Melbourne - 314</option>
-              <option value="208">Seven Hills - 208</option>
-              <option value="615">Perth - 615</option>
-              <option value="319">Ringwood - 319</option>
-              <option value="210">Sydney - 210</option>
-            </select>
-          </div>
+            <option value="213">Canberra - 213</option>
+            <option value="416">Fortitude Valley - 416</option>
+            <option value="710">Hobart - 710</option>
+            <option value="314">Melbourne - 314</option>
+            <option value="208">Seven Hills - 208</option>
+            <option value="615">Perth - 615</option>
+            <option value="319">Ringwood - 319</option>
+            <option value="210">Sydney - 210</option>
+          </select>
           <label htmlFor="customerName">Customer Name</label>
           <input
             type="text"
@@ -122,8 +120,6 @@ export default function CreateRequestForm({
             onChange={(e) => setName(e.target.value)}
             className="rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
-        </div>
-        <div>
           <label htmlFor="phone">Customer Phone</label>
           <input
             type="text"
@@ -133,8 +129,6 @@ export default function CreateRequestForm({
             onChange={(e) => setPhone(e.target.value)}
             className="rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
-        </div>
-        <div>
           <label htmlFor="email">Customer Email</label>
           <input
             type="text"
@@ -144,8 +138,6 @@ export default function CreateRequestForm({
             onChange={(e) => setEmail(e.target.value)}
             className="rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
-        </div>
-        <div>
           <label htmlFor="address">Customer Address</label>
           <textarea
             name="address"
@@ -156,12 +148,12 @@ export default function CreateRequestForm({
           />
         </div>
         Item info
-        <div className="md:col-span-2 bg-slate-300 flex flex-col justify-center items-center">
+        <div className="flex flex-col items-center justify-center bg-slate-300 md:col-span-2">
           <p>Request more items: </p>
           <button
             type="button"
             onClick={() => handleGetMoreItems()}
-            className="inline-block bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"
+            className="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base"
           >
             Get more requests
           </button>
@@ -204,10 +196,10 @@ export default function CreateRequestForm({
             <StockChecker sku={items[i].sku} />
           </div>
         ))}
-        <div className="md:col-span-2 flex flex-col justify-center items-center">
+        <div className="flex flex-col items-center justify-center md:col-span-2">
           <button
             type="submit"
-            className="inline-block bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"
+            className="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base"
           >
             Submit order/request to store
           </button>
