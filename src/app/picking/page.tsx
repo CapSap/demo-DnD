a83b2b0d3bd2b1e0475dda3746309c873117246e
CapSap <cshotam@gmail.com>
2024-07-05 14:35:57 +1000

    render client picking list component in the picking page/ route

--- src/app/picking/page.tsx
+++ src/app/picking/page.tsx
@@ -1,3 +1,11 @@
-export default async function PickingPage() {
-  return <div>pick page</div>;
+import PickingList from "../components/PickingList";
+
+export default function PickingPage() {
+  return (
+    <div>
+      <h1>Scan to pick page</h1>
+      <p>Please start scaning to pick items</p>
+      <PickingList />
+    </div>
+  );
 }
