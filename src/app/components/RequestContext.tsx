263f62224384d58fd01ed4b63990b41f66f5c452
CapSap <cshotam@gmail.com>
2024-07-14 12:59:43 +1000

    fix types- an array is being passed in- not the object
    also set defaultValue for context as null. not sure about this

--- src/app/components/RequestContext.tsx
+++ src/app/components/RequestContext.tsx
@@ -19,48 +19,39 @@ type IRequestContext = {
 };
 
 export const RequestContext = createContext<
-  [IRequestContext, React.Dispatch<SetStateAction<IRequestContext>>]
->([
-  {
-    _id: "1",
-    name: "Billy Bob",
-    phone: "123123123",
-    requestingStore: "Sydn",
-    email: "restsetgo@gmail.com",
-    address: "123 address road, NSW ,2000",
-    items: [
-      {
-        _id: "1",
-        sku: "506386",
-        description: "Mont Sleeping Bag Storage Sack",
-        quantity: "2",
-      },
-    ],
-  },
-  () => {},
-]);
+  [IRequestContext[], React.Dispatch<SetStateAction<IRequestContext[]>>] | null
+>(null);
 
 export default function RequestProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [picking, setPicking] = useState<IRequestContext>({
-    _id: "1",
-    name: "Billy Bob",
-    phone: "123123123",
-    requestingStore: "Sydn",
-    email: "restsetgo@gmail.com",
-    address: "123 address road, NSW ,2000",
-    items: [
-      {
-        _id: "1",
-        sku: "506386",
-        description: "Mont Sleeping Bag Storage Sack",
-        quantity: "2",
-      },
-    ],
-  });
+  const [picking, setPicking] = useState<IRequestContext[]>([
+    // im not sure if i want to set some inital default context, but ill try this and see
+    {
+      _id: "1",
+      name: "Billy Bob",
+      phone: "123123123",
+      requestingStore: "Sydn",
+      email: "restsetgo@gmail.com",
+      address: "123 address road, NSW ,2000",
+      items: [
+        {
+          _id: "1",
+          sku: "506386",
+          description: "Mont Sleeping Bag Storage Sack",
+          quantity: "2",
+        },
+        {
+          _id: "2",
+          sku: "50746702963NS",
+          description: "Mont Batwing Ultralight Thru-Hiker Tarp",
+          quantity: "3",
+        },
+      ],
+    },
+  ]);
 
   return (
     <RequestContext.Provider value={[picking, setPicking]}>
