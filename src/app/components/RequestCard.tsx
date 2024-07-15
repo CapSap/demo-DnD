9056241bf78942680bbcbdd18ca7824ff14fdd63
CapSap <cshotam@gmail.com>
2024-07-15 21:08:27 +1000

    add in the types for request card props

--- src/app/components/RequestCard.tsx
+++ src/app/components/RequestCard.tsx
@@ -1,26 +1,29 @@
+"use client";
+
 import { HydratedDocument } from "mongoose";
-import { IStoreRequest } from "../types/types";
-import { Item } from "../types/types";
+import { IStoreRequest, IStoreRequestFromDB, ItemDB } from "../types/types";
 import { Fragment } from "react";
 
 export default function RequestCard({
   request,
+  handleSelect,
 }: {
-  request: HydratedDocument<IStoreRequest>;
+  request: IStoreRequestFromDB;
+  handleSelect: (request: IStoreRequestFromDB) => {};
 }) {
   return (
-    <div className="min-w-72 border-2 border-slate-400 p-2">
+    <div id={request._id} className="min-w-72 border-2 border-slate-400 p-2">
+      <input type="checkbox" onChange={() => handleSelect(request)} />
       <p>
         Requesting Store: <strong>{request.requestingStore}</strong>
       </p>
       <p>
         Customer Name: <strong>{request.name}</strong>
       </p>
-      <p></p>
 
       <ul className="list-disc pl-6">
-        {request.items.map((item: Item) => (
-          <li key={item.id}>
+        {request.items.map((item: ItemDB) => (
+          <li key={item._id}>
             <p>
               {item.quantity} x {item.sku}
             </p>
