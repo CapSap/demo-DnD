aa8f0a9f74c39fa6bd4a1f48a9151e7adaebf348
CapSap <cshotam@gmail.com>
2024-08-01 14:24:39 +1000

    remove sqlite init code from route

--- src/app/api/prontoDatabase/route.ts
+++ src/app/api/prontoDatabase/route.ts
@@ -3,98 +3,29 @@ import Database from "better-sqlite3";
 import fs from "fs";
 import csv from "csv-parser";
 import path from "path";
+import { initializeProntoData, test } from "@/app/utils/initProntoDatabase";
 
 export async function GET(request: NextRequest) {
-  console.time("prontodb");
+  console.time("pronto data route");
 
-  // Create or open the SQLite database
-  const db = new Database("prontoData.db");
+  // this function should return an array of possible results or matches? from a search string (sku, barcode or product description)
 
-  // Function to initialize the database
-  const initializeDatabase = () => {
-    // Create a table (adjust the column definitions as needed)
-    db.prepare(
-      `CREATE TABLE IF NOT EXISTS prontoData (
-    ItemCode TEXT,
-    GTIN TEXT,
-    Style TEXT,
-    Colour TEXT,
-    Size TEXT,
-    Gender TEXT,
-    ItemCategoryCode TEXT,
-    ItemClassCode TEXT,
-    ABCClass TEXT,
-    Brand TEXT
-    )`,
-    ).run();
+  // so extract out what the search string is
 
-    // Prepare the insert statement
-    const insert = db.prepare(
-      'INSERT INTO prontoData ("ItemCode", GTIN, Style, Colour, Size, Gender, ItemCategoryCode, ItemClassCode, ABCClass, Brand) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-    );
+  // then check the db.
+  // to check the db, first if the db file needs to be updated.
+  //    to do this check, compare the length or number of rows between the csv and the .db file.
+  // if yes, re-create the db file
 
-    // Use a transaction for better performance
-    const insertMany = db.transaction((rows) => {
-      for (const row of rows) {
-        insert.run(
-          row.ItemCode,
-          row.GTIN,
-          row.Style,
-          row.Colour,
-          row.Size,
-          row.Gender,
-          row.ItemCategoryCode,
-          row.ItemClassCode,
-          row.ABCClass,
-          row.Brand,
-        );
-      }
-    });
+  // if not, then ready to proceed with check
+  // search the db for any results
 
-    console.log(process.cwd());
-    // Read the CSV file and insert the data into the database
-    return new Promise((resolve, reject) => {
-      const results = [];
-      const csvFilePath = path.join(
-        process.cwd(),
-        "/src/app/api/prontoDatabase/pronto-database.csv",
-      );
-      fs.createReadStream(csvFilePath)
-        .pipe(csv())
-        .on("data", (data) => results.push(data))
-        .on("end", () => {
-          insertMany(results);
-          console.log(
-            "CSV file successfully processed and data inserted into the database.",
-          );
-          resolve();
-        })
-        .on("error", (error) => {
-          console.error("Error processing CSV file:", error);
-          reject(error);
-        });
-    });
-  };
+  // return results
 
-  try {
-    // Initialize the database and wait for the CSV processing to complete
-    // await initializeDatabase();
+  initializeProntoData();
 
-    // Query the database (optional, for testing)
-    const queryDatabase = () => {
-      const rows = db.prepare("SELECT * FROM prontoData LIMIT 10").all();
-      for (const row of rows) {
-        console.log(row);
-      }
-    };
+  // test();
 
-    // Query the database
-    queryDatabase();
-  } finally {
-    // Close the database connection
-    db.close();
-  }
-
-  console.timeEnd("prontodb");
+  console.timeEnd("pronto data route");
   return NextResponse.json({ message: "CSV processed successfully" });
 }
