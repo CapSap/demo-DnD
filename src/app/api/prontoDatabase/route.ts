9d79822d8623184bc3507a9c40a300e82ec417f3
CapSap <cshotam@gmail.com>
2024-08-01 21:31:53 +1000

    use url params in route

--- src/app/api/prontoDatabase/route.ts
+++ src/app/api/prontoDatabase/route.ts
@@ -1,31 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
-import Database from "better-sqlite3";
-import fs from "fs";
-import csv from "csv-parser";
-import path from "path";
-import { initializeProntoData, test } from "@/app/utils/initProntoDatabase";
+import { initializeProntoData } from "@/app/utils/initProntoDatabase";
+import { exactSearch } from "@/app/utils/query";
 
 export async function GET(request: NextRequest) {
   console.time("pronto data route");
 
-  // this function should return an array of possible results or matches? from a search string (sku, barcode or product description)
+  await initializeProntoData();
 
-  // so extract out what the search string is
+  console.log("route has moved on");
 
-  // then check the db.
-  // to check the db, first if the db file needs to be updated.
-  //    to do this check, compare the length or number of rows between the csv and the .db file.
-  // if yes, re-create the db file
+  // Extract the search query from the request
+  const { searchParams } = new URL(request.url);
+  const searchString = searchParams.get("search") || "";
 
-  // if not, then ready to proceed with check
-  // search the db for any results
-
-  // return results
-
-  initializeProntoData();
-
-  // test();
+  // Call the query function
+  const results = exactSearch(searchString);
 
   console.timeEnd("pronto data route");
-  return NextResponse.json({ message: "CSV processed successfully" });
+  return NextResponse.json({ results });
 }
