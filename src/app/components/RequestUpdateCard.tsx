838881509b93ae141f0bc257221e70512251990c
CapSap <cshotam@gmail.com>
2024-09-01 01:05:42 +1000

    add input fields for ibt and tracking
    add a function to handle update and call server updateOne

--- src/app/components/RequestUpdateCard.tsx
+++ src/app/components/RequestUpdateCard.tsx
@@ -2,18 +2,28 @@
 
 import { HydratedDocument } from "mongoose";
 import { IStoreRequest, Item } from "../types/types";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 export default function RequestUpdateCard({
   request,
   handleSelect,
+  updateOneStoreRequest,
 }: {
   request: IStoreRequest;
   handleSelect: (request: IStoreRequest) => void;
+  updateOneStoreRequest: (request: string) => Promise<string>;
 }) {
+  const [ibt, setIbt] = useState(request.ibt);
+  const [tracking, setTracking] = useState(request.tracking);
+
+  async function handleUpdate() {
+    console.log("update button clicked ", { ...request, ibt, tracking });
+    const payload = JSON.stringify({ ...request, ibt, tracking });
+    const result = await updateOneStoreRequest(payload);
+  }
+
   return (
     <div id={request._id} className="min-w-72 border-2 border-slate-400 p-2">
-      <input type="checkbox" onChange={() => handleSelect(request)} />
       <p>
         Requesting Store: <strong>{request.requestingStore}</strong>
       </p>
@@ -34,13 +44,21 @@ export default function RequestUpdateCard({
       </ul>
       <div>
         <label htmlFor="ibt">IBT</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={ibt}
+          onChange={(e) => setIbt(e.target.value)}
+        />
       </div>
       <div>
         <label htmlFor="tracking">tracking</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={tracking}
+          onChange={(e) => setTracking(e.target.value)}
+        />
       </div>
-      <button>Update</button>
+      <button onClick={handleUpdate}>Update</button>
     </div>
   );
 }
