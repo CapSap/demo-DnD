7abac21451289a5edc971301d316316a3b040178
CapSap <cshotam@gmail.com>
2024-09-01 07:49:29 +1000

    ui and style improvements show a message when there are unsaved changes

--- src/app/components/RequestUpdateCard.tsx
+++ src/app/components/RequestUpdateCard.tsx
@@ -22,6 +22,9 @@ export default function RequestUpdateCard({
     const result = await updateOneStoreRequest(payload);
   }
 
+  const isThereUnsavedChanges =
+    ibt !== request.ibt || tracking !== request.tracking;
+
   return (
     <div id={request._id} className="min-w-72 border-2 border-slate-400 p-2">
       <p>
@@ -42,23 +45,53 @@ export default function RequestUpdateCard({
           </li>
         ))}
       </ul>
-      <div>
-        <label htmlFor="ibt">IBT</label>
+      <div className="grid grid-cols-4 items-center justify-items-end gap-y-1 p-1">
+        <label htmlFor="ibt" className="px-1">
+          IBT{" "}
+        </label>
         <input
+          className="col-span-3 rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           type="text"
           value={ibt}
           onChange={(e) => setIbt(e.target.value)}
         />
-      </div>
-      <div>
-        <label htmlFor="tracking">tracking</label>
+        <label htmlFor="tracking" className="px-0.5">
+          Tracking
+        </label>
         <input
+          className="col-span-3 rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           type="text"
           value={tracking}
           onChange={(e) => setTracking(e.target.value)}
         />
       </div>
-      <button onClick={handleUpdate}>Update</button>
+      {isThereUnsavedChanges ? (
+        <div className="flex items-center">
+          <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+            <svg
+              className="h-6 w-6 text-red-600"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+              />
+            </svg>
+          </div>
+          <p className="">Warning: There are some unsaved changes</p>
+        </div>
+      ) : null}
+      <button
+        className="rounded-lg bg-indigo-400 px-6 py-2 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-500 focus-visible:ring active:bg-indigo-700 md:text-base"
+        onClick={handleUpdate}
+      >
+        Update
+      </button>
     </div>
   );
 }
