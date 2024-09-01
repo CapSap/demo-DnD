8bd49a017371c2208be45d2686b80dd9de2f6275
CapSap <cshotam@gmail.com>
2024-09-01 17:26:28 +1000

    create new component for archived (nothing further to do requests)

---
+++ src/app/components/RequestCardArchived.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { HydratedDocument } from "mongoose";
+import { IStoreRequest, Item } from "../types/types";
+import { Fragment } from "react";
+
+export default function RequestCard({ request }: { request: IStoreRequest }) {
+  console.log(request);
+  return (
+    <div id={request._id} className="min-w-96 border-2 border-slate-400 p-2">
+      <p>
+        Requesting Store: <strong>{request.requestingStore}</strong>
+      </p>
+      <p>
+        Customer Name: <strong>{request.name}</strong>
+      </p>
+
+      <ul className="list-disc pl-6">
+        {request.items.map((item: Item) => (
+          <li key={item._id}>
+            <p>
+              {item.quantity} x {item.sku}
+            </p>
+            <p>{item.description}</p>
+            <p>qty picked: {item.quantityPicked}</p>
+          </li>
+        ))}
+      </ul>
+      <p>
+        IBT: <strong>{request.ibt}</strong>
+      </p>
+      <p>
+        Tracking: <strong>{request.tracking}</strong>
+      </p>
+      <p>
+        Posted on: <strong>{request.updatedAt}</strong>
+      </p>
+    </div>
+  );
+}
