e69bab829bf2f833156b9a67017089e0385a6520
CapSap <cshotam@gmail.com>
2024-09-12 16:21:12 +1000

    remove picking logic things from generic request card

--- src/app/components/RequestCard.tsx
+++ src/app/components/RequestCard.tsx
@@ -4,31 +4,19 @@ import { HydratedDocument } from "mongoose";
 import { IStoreRequest, Item } from "../types/types";
 import { Fragment } from "react";
 
-export default function RequestCard({
-  request,
-  handleSelect,
-}: {
-  request: IStoreRequest;
-  handleSelect: (request: IStoreRequest) => void;
-}) {
+export default function RequestCard({ request }: { request: IStoreRequest }) {
   return (
     <div className="m-2 min-w-96 justify-center border-2 border-slate-400 p-2">
-      <div className="flex items-center rounded-lg bg-pink-200 hover:bg-pink-300 active:bg-pink-400">
-        <input
-          id={request._id}
-          className="m-2 scale-150"
-          type="checkbox"
-          onClick={() => handleSelect(request)}
-        />
-        <label className="ml-2 w-full" htmlFor={request._id}>
-          Click me to pick this order
-        </label>
-      </div>
-      <p>
-        Requesting Store: <strong>{request.requestingStore}</strong>
-      </p>
       <p>
         Customer Name: <strong>{request.name}</strong>
+      </p>
+
+      <p>
+        Customer Email: <strong>{request.email}</strong>
+      </p>
+
+      <p>
+        Customer Phone: <strong>{request.phone}</strong>
       </p>
 
       <ul className="list-disc pl-6">
@@ -38,7 +26,6 @@ export default function RequestCard({
               {item.quantity} x {item.sku}
             </p>
             <p>{item.description}</p>
-            <p>Qty picked: {item.quantityPicked}</p>
           </li>
         ))}
       </ul>
