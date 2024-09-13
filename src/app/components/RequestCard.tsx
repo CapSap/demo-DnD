a271a647325e6b22dbfc6dfac374ead35d0e7190
CapSap <cshotam@gmail.com>
2024-09-13 12:09:40 +1000

    add request status to the request card add tracking and ibt to tracking card, with tracking link

--- src/app/components/RequestCard.tsx
+++ src/app/components/RequestCard.tsx
@@ -8,6 +8,10 @@ export default function RequestCard({ request }: { request: IStoreRequest }) {
   return (
     <div className="m-2 min-w-96 justify-center border-2 border-slate-400 p-2">
       <p>
+        Request Status: <strong>{request.status}</strong>
+      </p>
+
+      <p>
         Customer Name: <strong>{request.name}</strong>
       </p>
 
@@ -29,6 +33,26 @@ export default function RequestCard({ request }: { request: IStoreRequest }) {
           </li>
         ))}
       </ul>
+      {request.tracking ? (
+        <div className="flex gap-2">
+          <p>
+            Tracking number: <strong>{request.tracking}</strong>
+          </p>
+          <a
+            className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+            href={`https://auspost.com.au/mypost/track/details/${request.tracking}`}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Tracking link
+          </a>
+        </div>
+      ) : null}
+      {request.ibt ? (
+        <p>
+          IBT: <strong>{request.ibt}</strong>
+        </p>
+      ) : null}
     </div>
   );
 }
