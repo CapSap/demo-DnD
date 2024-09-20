eb54c35cc0e4c69d22272a1b62c0201270413ec6
CapSap <cshotam@gmail.com>
2024-09-20 10:51:09 +1000

    make the button smaller and change its colour

--- src/app/components/ConfirmButton.tsx
+++ src/app/components/ConfirmButton.tsx
@@ -20,14 +20,14 @@ const ConfirmButton = ({ onConfirm }) => {
     <div>
       <button
         onClick={handleClick}
-        className="rounded bg-indigo-500 px-4 py-2 font-bold text-white hover:bg-indigo-700"
+        className="rounded bg-orange-500 px-2 py-1 text-sm text-white hover:bg-yellow-700"
       >
-        {confirming ? "Are you sure?" : "Click Me"}
+        {confirming ? "Are you sure?" : "Redo tracking or ibt"}
       </button>
       {confirming && (
         <button
           onClick={handleCancel}
-          className="ml-2 rounded bg-gray-500 px-4 py-2 font-bold text-white hover:bg-gray-700"
+          className="ml-2 rounded bg-gray-500 px-2 py-1 text-sm text-white hover:bg-gray-700"
         >
           Cancel
         </button>
