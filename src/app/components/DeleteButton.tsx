c1b084eef60da42a2f612dad0883e74811552f2e
CapSap <cshotam@gmail.com>
2024-09-24 12:37:15 +1000

    add nice and large warning about calling the store

--- src/app/components/DeleteButton.tsx
+++ src/app/components/DeleteButton.tsx
@@ -23,21 +23,32 @@ const ConfirmButton = ({
   };
 
   return (
-    <div>
-      <button
-        onClick={handleClick}
-        className="rounded bg-red-600 px-2 py-1 text-sm text-white hover:bg-red-800 active:bg-red-950"
-      >
-        {confirming ? "Are you sure? This cannot be undone!" : buttonText}
-      </button>
-      {confirming && (
+    <div className="mt-2">
+      <div className="">
         <button
-          onClick={handleCancel}
-          className="ml-2 rounded bg-gray-500 px-2 py-1 text-sm text-white hover:bg-gray-700"
+          onClick={handleClick}
+          className="rounded bg-red-600 px-2 py-1 text-sm text-white hover:bg-red-800 active:bg-red-950"
         >
-          Cancel
+          {confirming ? "Are you sure?" : buttonText}
         </button>
-      )}
+        {confirming && (
+          <button
+            onClick={handleCancel}
+            className="ml-2 rounded bg-gray-500 px-2 py-1 text-sm text-white hover:bg-gray-700"
+          >
+            Cancel
+          </button>
+        )}
+      </div>
+      {confirming ? (
+        <div
+          className="mt-1 max-w-sm border-l-4 border-orange-500 bg-orange-100 px-2 py-1 text-orange-700"
+          role="alert"
+        >
+          <p className="font-bold">Have you called the store?</p>
+          <p>The store needs to be aware that the request is being cancelled</p>
+        </div>
+      ) : null}
     </div>
   );
 };
