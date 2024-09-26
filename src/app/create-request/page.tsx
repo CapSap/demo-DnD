5974ab926129005e099a87c8b35cbce9a0ffe5f0
CapSap <cshotam@gmail.com>
2024-09-26 17:08:56 +1000

    try fetching blob from server

--- src/app/create-request/page.tsx
+++ src/app/create-request/page.tsx
@@ -1,18 +1,32 @@
 import CreateRequestForm from "../components/CreateRequestForm";
 import { IPartialStoreRequest, IStoreRequest } from "../types/types";
 import { createStoreRequest as dbCreate } from "../utils/dbConnect";
+import { list } from "@vercel/blob";
 
-export default function CreateRequestPage() {
+export default async function CreateRequestPage() {
   async function createStoreRequest(request: IPartialStoreRequest) {
     "use server";
     const response = await dbCreate(request);
-
     if (response.error) {
       return `Error creating store request: ${response.error.message}. Details: ${response.error.details}`;
     }
 
     return `Store request created successfully!`;
   }
+
+  const data = await list();
+  const dlUrl = data.blobs[0].downloadUrl;
+  console.log("BLOB DATA");
+
+  async function fetchCSV() {
+    const res = await fetch(dlUrl);
+    const csvData = await res.text();
+    return csvData;
+  }
+
+  const testing = await fetchCSV();
+
+  console.log("testing", testing);
 
   return (
     <div>
